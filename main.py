81ec26f1cd0d38d3757a5031246c3c23d6188c6c
maelou363 <lmwressell@aol.com>
2021-03-20 14:26:56 +0000

    cleaned data and created scatterplot

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 
 # API - not necessary to need to have both though for this dataset.
 # from kaggle.api.kaggle_api_extended import KaggleApi
@@ -6,10 +9,10 @@ import pandas as pd
 # api.authenticate()
 # api.competitions_list(category='all')
 
-#Import csv
+# Import csv
 data = pd.read_csv("StudentsPerformance.csv")
 
-### Going through and inspecting the dataset ###
+# Going through and inspecting the dataset ###
 # print(data.columns)
 # print(data.info())
 # print(data.shape)
@@ -19,16 +22,31 @@ data = pd.read_csv("StudentsPerformance.csv")
 # print(data[['parental-education', 'gender', 'race', 'math-score']])
 # print(data.iloc[2])
 
+# Iterrating into each individual dataset
 # for index, row in data.iterrows():
 #     print(index, row)
 
+
+# loc to find only high school education from parents
 # print(data.loc[data['parental-education'] == 'high school'])
 # print(data.describe())
 # print(data.sort_values(['math-score', 'reading-score', 'writing-score'], ascending=False))
 
-#### Creating new column called Avg-Scores to average out the scores in an attempt to see the correlation between test scores and social dynamics ###
-data['Avg-Score'] = data['math-score'] + data['reading-score'] + data['writing-score'] / 3
-print(data.head())
+# Creating new column called Avg-Scores to average out the scores in an attempt to see the correlation
+# between test scores and social dynamics #
+data['Avg-Score'] = data['math-score'] + data['reading-score'] + data['writing-score']
+# print(data.head(5))
+
+# Dropping columns that does not pertain for this research
+data_new = data.drop(columns=['gender', 'race', 'lunch', 'preparation-course', 'math-score', 'reading-score', 'writing-score'])
+(data_new.sort_values(['parental-education', 'Avg-Score'], ascending=False))
+
+fig, ax = plt.subplots(figsize=(10, 10))
+ax.scatter(x = data_new['parental-education'], y = data_new['Avg-Score'])
+plt.xlabel('Parental Education')
+plt.ylabel('Childs Avg Score')
+plt.show()
+
 
 
 
